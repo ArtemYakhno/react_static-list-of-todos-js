2cb64f5d1938909d550e08fe31e3911a1ca32990
Artem Yakhno <artemen23093@gmail.com>
2025-11-10 14:05:37 +0200

    added solutionn 2

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,4 @@
-import { UserInfo } from '../UserInfo/UserInfo';
+import { UserInfo } from '../UserInfo';
 
 // Add the required props
 export const TodoInfo = ({ todo }) => (
@@ -7,6 +7,6 @@ export const TodoInfo = ({ todo }) => (
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
-    {todo.user && <UserInfo key={todo.user.id} user={todo.user} />}
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
